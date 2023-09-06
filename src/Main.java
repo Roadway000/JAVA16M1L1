7fdbe8d7a38185f0dc4e575d36d4baa4a86d226b
Roadway000 <roadway000@gmail.com>
2023-09-06 21:01:45 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        String sVar = "";
-        printToConsole(sVar);
+    public char[][] createKeyboard() {
+        return new char[][]  {
+                {'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'},
+                {'*', '0', '#'}
+        };
     }
-    public static void printToConsole(String args) {
-        System.out.println("Hello world!");
+    public void printKeyboard() {
+        //char[][] arr = new Main().createKeyboard();
+        System.out.println(Arrays.toString(new Main().createKeyboard()[0]));
+        System.out.println(Arrays.toString(new Main().createKeyboard()[1]));
+        System.out.println(Arrays.toString(new Main().createKeyboard()[2]));
+        System.out.println(Arrays.toString(new Main().createKeyboard()[3]));
+
+//        for (char[] tmp : arr) {
+//            System.out.println(Arrays.toString(tmp));
+//        };
+//        for (int i=0; i<arr.length;i++) {
+//            System.out.println(Arrays.toString(arr[i]));
+//        };
+    }
+    public static void main(String[] args) {
+        new Main().printKeyboard();
     }
 }
