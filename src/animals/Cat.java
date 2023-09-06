9a0eb7de8e1da2b992242fe67ff32b61ca1f37f0
Roadway000 <roadway000@gmail.com>
2023-09-06 21:03:42 +0300

    Initial commit

---
+++ src/animals/Cat.java
@@ -0,0 +1,10 @@
+package animals;
+public class Cat {
+    String name;
+    /**
+     * This is method which will Cat.Mew
+     */
+    public void Mew() {
+        System.out.println("Mew.. Mew.. My name is "+name);
+    }
+}
